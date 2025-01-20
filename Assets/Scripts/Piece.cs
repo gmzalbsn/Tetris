dc16ca7b88282928a51567ad87e617816b608700
gmzalbsn <gamzealbsn@gmail.com>
2025-01-20 19:35:26 +0300

    fix: The issue of leaving the board during rotation has been resolved

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Piece : MonoBehaviour
 {
@@ -8,12 +9,19 @@ public class Piece : MonoBehaviour
     public Vector3Int [] cells { get;private set; }
     public Vector3Int position { get;private set; }
     public int rotationIndex{ get;private set; }
+
+    public float stepDelay = 1;
+    public float lockDelay = 0.5f;
+    private float stepTime;
+    private float lockTime;
     public void Initialize(Board board ,Vector3Int position,TetrominoData data)
     {
         this.board = board;
         this.position = position;
         this.data = data;
         rotationIndex = 0;
+        stepTime=Time.time+stepDelay;
+        lockTime= 0f;
         if (cells == null)
         {
             cells = new Vector3Int[data.cells.Length];
@@ -28,6 +36,7 @@ public class Piece : MonoBehaviour
     private void Update()
     {
         board.Clear(this);
+        lockTime += Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.Q))
         {
             Rotate(-1);
@@ -53,8 +62,18 @@ public class Piece : MonoBehaviour
         {
             HardDrop();
         }
+
+        if (Time.time>=stepTime)
+        {
+            Step();
+        }
         board.Set(this);
         
+    }
+
+    private void Step()
+    {
+        stepTime=Time.time+stepDelay;
     }
 
     private void HardDrop()
@@ -74,14 +93,26 @@ public class Piece : MonoBehaviour
        if (valid)
        {
            position = newPosition;
+           lockTime = 0f;
        }
        return valid;
     }
 
     private void Rotate(int direction)
     {
-        rotationIndex+= Wrap(rotationIndex+direction,0,4);
+        int originalRotationIndex = rotationIndex;
+        rotationIndex= Wrap(rotationIndex+direction,0,4);
+        ApplyRotationMatrix(direction);
 
+        if (!TestWallKicks(rotationIndex,direction))
+        {
+            rotationIndex=originalRotationIndex;
+            ApplyRotationMatrix(-direction);
+        }
+    }
+
+    private void ApplyRotationMatrix(int direction)
+    {
         for (int i = 0; i < data.cells.Length; i++)
         {
             Vector3 cell= cells[i];
@@ -90,9 +121,9 @@ public class Piece : MonoBehaviour
             switch (data.tetromino)
             {
                 case Tetromino.I:
-                    case Tetromino.O:
-                        cell.x -= 0.5f;
-                        cell.y-= 0.5f;
+                case Tetromino.O:
+                    cell.x-= 0.5f;
+                    cell.y-= 0.5f;
                     x =Mathf.CeilToInt((cell.x * Data.RotationMatrix[0]*direction) + (cell.y * Data.RotationMatrix[1]*direction));
                     y =Mathf.CeilToInt(cell.x *Data.RotationMatrix[2]*direction + (cell.y*Data.RotationMatrix[3]*direction));
                     break;
@@ -105,7 +136,32 @@ public class Piece : MonoBehaviour
             cells[i] = new Vector3Int(x, y, 0);
         }
     }
+    private bool TestWallKicks(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex=GetWallKickIndex(rotationIndex,rotationDirection);
+        for (int i = 0; i < data.wallKicks.GetLength(1); i++)
+        {
+            Vector2Int translation= data.wallKicks[wallKickIndex,i];
 
+            if (Move(translation))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int GetWallKickIndex(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex = rotationIndex * 2;
+
+        if (rotationDirection<0)
+        {
+            wallKickIndex--;
+        }
+
+        return Wrap(wallKickIndex, 0, data.wallKicks.GetLength(0));
+    }
     private int Wrap(int input, int min, int max)
     {
         if (input<min)
@@ -114,7 +170,7 @@ public class Piece : MonoBehaviour
         }
         else
         {
-            return max+(input-min)% (max-min);
+            return min +(input-min)% (max-min);
         }
     }
 }
